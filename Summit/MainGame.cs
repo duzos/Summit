eb7074f1e990197a43fbdac314e1d4721f46f336
James Hall <james.hall01@outlook.com>
2025-11-11 09:50:27 +0000

    Add project files.

---
+++ Summit/MainGame.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SummitKit;
+
+namespace Summit
+{
+    public class MainGame : Core
+    {
+        private Texture2D _test;
+
+        public MainGame() : base("Summit", 1280, 720, false)
+        {
+
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+
+            _test = Content.Load<Texture2D>("assets/ace");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.AliceBlue);
+
+            SpriteBatch.Begin();
+            MouseState mouse = Mouse.GetState();
+            SpriteBatch.Draw(_test, new Vector2(mouse.X - (_test.Width / 2), mouse.Y - (_test.Height / 2)), Color.White);
+            SpriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Summit/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new Summit.MainGame();
+game.Run();
